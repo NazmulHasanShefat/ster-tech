aa6a2fc051fa23de5d18a13865964fd7a2eb653b
NazmulHasanShefat <119728546+NazmulHasanShefat@users.noreply.github.com>
2024-06-14 17:36:34 +0600

    frist commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['*'],
+  theme: {
+    extend: {
+      colors:{
+        "primary-color":"#000000",
+        "hover-color":"#Ef4A23",
+        "text-color-2":"#ffffff80",
+      },
+      height:{
+        "image-height":"160px"
+      }
+    },
+  },
+  plugins: [],
+}
+
