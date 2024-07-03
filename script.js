1b5bbc6ed892e80ff62fcb473a4edb5b15083c9d
NazmulHasanShefat <119728546+NazmulHasanShefat@users.noreply.github.com>
2024-07-03 15:57:02 +0600

    last commit

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+ // If media query matches
 let humberger = document.querySelector(".menu-icon");
 let main_menu = document.querySelector(".nav-2");
 // sublist 
@@ -98,6 +99,20 @@ const li_ram_laptop = document.querySelector(".li_ram_laptop");
 const ul_ram_laptop = document.querySelector(".ul_ram_laptop");
 const li_power_supply = document.querySelector(".li_power_supply");
 const ul_power_supply = document.querySelector(".ul_power_supply");
+const li_HardDisk = document.querySelector(".li_Harddisk");
+const ul_HardDisk = document.querySelector(".ul_Harddisk");
+const li_Portable_Hdd = document.querySelector(".li_Portable_HDD");
+const ul_Portable_HDD = document.querySelector(".ul_Portable_HDD");
+const li_SSD = document.querySelector(".li_SSD");
+const ul_SSD = document.querySelector(".ul_SSD");
+const li_Portable_SSD = document.querySelector(".li_Portable_SSD");
+const ul_Portable_SSD = document.querySelector(".ul_Portable_SSD");
+const li_casing = document.querySelector(".li_casing");
+const ul_casing = document.querySelector(".ul_casing");
+const li_casing_cooler = document.querySelector(".li_casing_cooler");
+const ul_casing_cooler = document.querySelector(".ul_casing_cooler");
+const li_optical_driver = document.querySelector(".li_optical_driver");
+const ul_optical_driver = document.querySelector(".ul_optical_driver");
 
 li_component.addEventListener("click",()=>{
     ul_component.classList.toggle("hidden");
@@ -126,3 +141,48 @@ li_ram_laptop.addEventListener("click",()=>{
 li_power_supply.addEventListener("click",()=>{
     ul_power_supply.classList.toggle("hidden");
 })
+li_HardDisk.addEventListener("click",()=>{
+    ul_HardDisk.classList.toggle("hidden");
+})
+li_Portable_Hdd.addEventListener("click",()=>{
+    ul_Portable_HDD.classList.toggle("hidden");
+})
+li_SSD.addEventListener("click",()=>{
+    ul_SSD.classList.toggle("hidden");
+})
+li_Portable_SSD.addEventListener("click",()=>{
+    ul_Portable_SSD.classList.toggle("hidden");
+})
+li_casing.addEventListener("click",()=>{
+    ul_casing.classList.toggle("hidden");
+})
+li_casing_cooler.addEventListener("click",()=>{
+    ul_casing_cooler.classList.toggle("hidden");
+})
+li_optical_driver.addEventListener("click",()=>{
+    ul_optical_driver.classList.toggle("hidden");
+})
+
+// monitor
+const li_monitor = document.querySelector(".li_monitor");
+const ul_monitor = document.querySelector(".ul_monitor");
+const li_UPS = document.querySelector(".li_UPS");
+const ul_UPS = document.querySelector(".ul_UPS");
+const li_phone = document.querySelector(".li_phone");
+const ul_phone = document.querySelector(".ul_phone");
+const li_tablet = document.querySelector(".li_tablet");
+const ul_tablet = document.querySelector(".ul_tablet");
+
+li_monitor.addEventListener("click",()=>{
+    ul_monitor.classList.toggle("hidden");
+})
+li_UPS.addEventListener("click",()=>{
+    ul_UPS.classList.toggle("hidden");
+})
+li_phone.addEventListener("click",()=>{
+    ul_phone.classList.toggle("hidden");
+})
+li_tablet.addEventListener("click",()=>{
+    ul_tablet.classList.toggle("hidden");
+})
+
